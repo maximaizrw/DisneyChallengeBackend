81913ae2237550efe8c04a1ebfbef2197f6c3892
Maximiliano Maiz <maximaizrw@gmail.com>
2024-05-27 14:14:15 -0300

    Init

---
+++ src/main/java/com/application/rest/disney/DisneyChallengeApiApplication.java
@@ -0,0 +1,13 @@
+package com.application.rest.disney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisneyChallengeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisneyChallengeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/rest/disney/DisneyChallengeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.rest.disney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisneyChallengeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
